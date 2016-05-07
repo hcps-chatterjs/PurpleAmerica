11599a2630e88c5afe9d760c4a789027e33f0ef4
hcps-chatterjs <hcps-chatterjs@henricostudents.org>
2016-05-06 22:25:25 -0400

    Added files via upload

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -22,11 +22,12 @@ import java.util.Scanner;
  */
 public class PoliticalMap {
     public static void main(String[] args) throws FileNotFoundException{;
-        File file = new File("C:\\Users\\hcps-larochesp\\Documents\\PurpleAmericaProject\\PurpleAmerica\\src\\data\\USA-county.txt");
+        File file = new File("C:\\Users\\hcps-chatterjs\\Documents\\FIFA II\\New FOrk\\PurpleAmerica1\\src\\data\\USA-county.txt");
         Scanner scan = new Scanner(file);
+        StdDraw.setPenColor(Color.red);
         StdDraw.setCanvasSize(1200, 650);
         StdDraw.setPenRadius(0.002);
-        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.setPenColor(Color.green);
         double xMin = scan.nextDouble();
         double yMin = scan.nextDouble();
         double xMax = scan.nextDouble();
@@ -38,6 +39,7 @@ public class PoliticalMap {
         double[] yVal;
         int pointNum = 0;
         
+        
         for(int i = 0; i < regionNum; i++){
             if(scan.hasNextInt() == true){
                 pointNum = scan.nextInt();
@@ -47,13 +49,30 @@ public class PoliticalMap {
                    xVal[j] = scan.nextDouble();
                    yVal[j] = scan.nextDouble();
                 }
+              
                 StdDraw.polygon(xVal, yVal);
+                
+              
             }
             else{
                 i = i - 1;
                 scan.next();
             }
         }
+        while(true) {
+            if(StdDraw.mousePressed()) {
+                System.out.println(StdDraw.mouseX() + " " + StdDraw.mouseY());
+                StdDraw.point(StdDraw.mouseX() , StdDraw.mouseY());
+                
+                try {
+                    Thread.sleep(1000);
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
         
+       
     }
-}//Real
+   
+}
